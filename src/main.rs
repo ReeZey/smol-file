47a3d6b7d2a80fa6fa2dd04bd99d0ac4d7c5a140
ReeZey <johannes@seved.net>
2024-04-13 10:31:15 +0200

    primitive version 1

--- src/main.rs
+++ src/main.rs
@@ -1,100 +1,19 @@
-use std::fs;
+mod utils;
+mod smolblob;
 
-const ALPHABET: &str = " abcdefghijklmnopqrstuvwxyz.!?12";
+use std::fs;
+use smolblob::SmolBlob;
 
 fn main() {
-    let input = "aa".to_owned();
-
-    //let input = fs::read_to_string("bible2022.txt").unwrap();
-    println!("{}", input.len());
-
+    let input = "test".to_owned();
+    //println!("INPUT LENGTH: {}", input.len());
     fs::write("raw.bin", &input).unwrap();
 
-    let encoded = encode(&input);
-    println!("encoded len: {}", encoded.len());
+    let encoded = SmolBlob::encode(&input);
+    fs::write("smol.bin", &encoded.buffer()).unwrap();
 
-    fs::write("smol.bin", &encoded).unwrap();
-
-    let decoded = decode(encoded);
-    println!("decoded len: {}", decoded.len());
-    println!("decoded text: {:?}", decoded);
-    //fs::write("out.bin", result).unwrap();
-
+    let decoded = SmolBlob::decode(&mut encoded.buffer()).unwrap();
+    //println!("decoded len: {}", decoded.len());
+    //println!("decoded text: {:?}", decoded);
     fs::write("unsmol.bin", decoded).unwrap();
-}
-
-fn encode(str: &String) -> Vec<u8> {
-    let mut buffer = vec![];
-
-    let mut current: u16 = 0;
-    let mut offset: usize = 0;
-    for char in str.to_lowercase().chars().into_iter() {
-        let index = char_to_index(char);
-        current |= (index as u16) << offset;
-        offset += 5;
-        if offset >= 8 {
-            buffer.push((current & 0xff) as u8);
-            current = (current & 0xff00) >> 8;
-            offset -= 8;
-        }
-    }
-    //println!("{}", offset);
-    if offset > 0 {
-        buffer.push(current as u8);
-    }
-
-    return buffer;
-}
-
-fn decode(input: Vec<u8>) -> String {
-    if input.len() == 0 {
-        panic!("decode length is zero");
-    }
-
-    
-    let mut out_string = String::new();
-    let mut offset: usize = 0;
-    loop {
-        let current_byte_index = offset / 8;
-        if current_byte_index >= input.len() {
-            break;
-        }
-        let first_byte: u8 = input[current_byte_index];
-        let secound_byte: u8 = if current_byte_index + 1 < input.len() {
-            input[current_byte_index + 1]
-        } else {
-            0
-        };
-        let current: u16 = ((secound_byte as u16) << 8) | first_byte as u16;
-        
-        let formatted = format!("{:016b}", current);
-        let buety = formatted.split_at(8);
-        println!("{:?}", buety);
-
-        let char_index = (current & (31 << (offset%8))) >> (offset%8);
-        let char = index_to_char(char_index as usize);
-        println!("{:?}, at offset {}", char, offset);
-        out_string.push(char);
-        offset += 5;
-    }
-
-    out_string
-}
-
-fn char_to_index(search_char: char) -> usize {
-    for (index, char) in ALPHABET.chars().enumerate() {
-        if search_char == char {
-            return index;
-        }
-    }
-    return 0;
-}
-
-fn index_to_char(search_index: usize) -> char {
-    for (index, char) in ALPHABET.chars().enumerate() {
-        if search_index == index {
-            return char;
-        }
-    }
-    return ALPHABET.chars().nth(0).unwrap();
 }
